8d69cf13e4e97adbaba976ac8c6f33f3d0656dec
mingi <mintim01@naver.com>
2024-04-29 23:38:55 +0900

    feat: 로그인 기능 및 간단한 html 구현

--- project/project/urls.py
+++ project/project/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
-from rest_framework import routers, serializers, viewsets
 from addresses import views
+from django.contrib import admin
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('addresses/', views.address_list),
     path('addresses/<int:pk>/', views.address),
+    path('login/', views.login),
+    path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
