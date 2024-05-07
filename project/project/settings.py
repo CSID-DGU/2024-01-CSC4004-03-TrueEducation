4a9d8074e99da61e3d8601bc481d859ca728391b
mingi <mintim01@naver.com>
2024-05-07 17:03:39 +0900

    feat: ngrok 서비스를 이용한 외부 서버 접속 기능 개발

--- project/project/settings.py
+++ project/project/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
-from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -49,6 +48,8 @@ INSTALLED_APPS = [
 
     'allauth',
     'allauth.account',
+
+    'corsheaders',
 ]
 
 # users 앱에서 내가 설정한 User를 사용하겠다고 설정한다.
@@ -138,6 +139,8 @@ REST_FRAMEWORK = {
 }
 '''
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -222,3 +225,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ORIGIN_ALLOW_ALL = True # <- 모든 호스트 허용
