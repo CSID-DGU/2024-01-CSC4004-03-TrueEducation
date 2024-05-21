5c574e797ba40e5ff73717df87972ba0b5284e18
mingi <mintim01@naver.com>
2024-05-21 15:28:16 +0900

    fix: url 경로 단순화

--- project/project/urls.py
+++ project/project/urls.py
@@ -17,5 +17,5 @@ Including another URLconf
 from django.urls import path, include
 
 urlpatterns = [
-    path('users/', include('users.urls')),
+    path('', include('users.urls')),
 ]
--- project/users/urls.py
+++ project/users/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 
 app_name = "users"
 urlpatterns = [
-    path('dj-rest-auth/', include('dj_rest_auth.urls')),
-    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls'))
+    path('', include('dj_rest_auth.urls')),
+    path('registration/', include('dj_rest_auth.registration.urls'))
 ]
